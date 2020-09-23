c0acf416977a6a9fe64066809853f78704d41b00
Saroj Dahal <sarojdahal1213@gmail.com>
2020-09-23 23:37:47 +0545

    added : About page

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+import BreadCrumb from "../components/BreadCrumb";
+
+const About = () => {
+  return (
+    <div>
+      <BreadCrumb menu="about" />
+    </div>
+  );
+};
+
+export default About;
