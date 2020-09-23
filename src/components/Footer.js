9f26e6d77bcb9517ddd40bd1f3f38ba079c8bb42
Saroj Dahal <sarojdahal1213@gmail.com>
2020-09-23 23:36:43 +0545

    added: Footer.js (component)

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../assets/css/Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <h2 className="footer-main-title">HackObterFest Nepal</h2>
+      <br />
+      <br />
+      Copyright &copy; {new Date().getFullYear()}
+    </div>
+  );
+};
+
+export default Footer;
