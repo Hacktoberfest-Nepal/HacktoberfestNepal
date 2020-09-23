01cea1b64711bca1080204a098f893e811de7b10
Saroj Dahal <sarojdahal1213@gmail.com>
2020-09-23 23:40:24 +0545

    added : Routings for Navbar, and footer for page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Speakers from "./pages/Speakers";
+import Contact from "./pages/Contact";
+import Footer from "./components/Footer";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/speakers" component={Speakers} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
+
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
