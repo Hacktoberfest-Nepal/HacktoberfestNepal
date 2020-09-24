a1236686db8dfb27f72e07c40f5416c8692bf81d
AnjalBam <anjalbam81@gmail.com>
2020-09-24 16:31:09 +0545

    feat: add event homepage component and style

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,11 @@
 import React from "react";
+import EventComponent from "../components/EventComponent";
 import TimeOutComponent from "../components/TimeOutComponent";
 
 const Home = () => {
   return (
     <div>
-      <p>Home </p> <TimeOutComponent />
+      <p>Home </p> <TimeOutComponent /> <EventComponent />
     </div>
   );
 };
