72c6598d42e0a6f31ef7a3270d93274b844c8450
Saroj Dahal <sarojdahal1213@gmail.com>
2020-09-26 16:39:18 +0545

    added: Speakers.js component

--- src/pages/Speakers.js
+++ src/pages/Speakers.js
@@ -1,7 +1,14 @@
 import React from "react";
+import BreadCrumb from "../components/BreadCrumb";
+import SpeakerList from "../components/speakers/SpeakerList";
 
 const Speakers = () => {
-  return <div>Speakers</div>;
+  return (
+    <div>
+      <BreadCrumb menu="speakers" />
+      <SpeakerList />
+    </div>
+  );
 };
 
 export default Speakers;
