7ccfed676da1ba0c266de60fa7902590d80b0fac
Saroj Dahal <sarojdahal1213@gmail.com>
2020-09-27 19:23:27 +0545

    added: Organizers Component in About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,12 +1,14 @@
 import React from "react";
 import BreadCrumb from "../components/BreadCrumb";
 import SponsorComponent from "../components/about/Sponsor";
+import OrganizersComponent from "../components/about/Organizers";
 
 const About = () => {
   return (
     <div>
-      <BreadCrumb menu="about" />
+      <BreadCrumb image="about.png" title="About" description="All about Hacktoberfest" />
       <SponsorComponent />
+      <OrganizersComponent />
     </div>
   );
 };
