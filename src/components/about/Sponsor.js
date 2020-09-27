896d14f89483b3346e9dca442b96f7e9bb81830d
Saroj Dahal <sarojdahal1213@gmail.com>
2020-09-27 19:28:08 +0545

    added: want-to-be sponser section

--- src/components/about/Sponsor.js
+++ src/components/about/Sponsor.js
@@ -1,23 +1,31 @@
 import React from "react";
 import "../../assets/css/SponsorComponent.css";
+import TitleComponent from "../Title";
 
 const Sponsor = () => {
   return (
-    <div className="container sponsor p-4">
-      <h2 class="shadow-title">Sponsors</h2>
-      <hr class="default-hr" />
-      <b>We are grateful to our amazing partners and sponsors</b>
+    <div className="container sponsor">
+
+      <TitleComponent
+        title="Sponsors"
+        description="We are grateful to our amazing partners and sponsors"
+      />
+
       <div className="sponsorList mt-3">
-        <img src="https://news.aut.ac.nz/__data/assets/image/0006/92328/placeholder-image10.jpg" />
+        <img class="card" src="https://news.aut.ac.nz/__data/assets/image/0006/92328/placeholder-image10.jpg" />
         <img src="https://news.aut.ac.nz/__data/assets/image/0006/92328/placeholder-image10.jpg" />
         <img src="https://news.aut.ac.nz/__data/assets/image/0006/92328/placeholder-image10.jpg" />
         <img src="https://news.aut.ac.nz/__data/assets/image/0006/92328/placeholder-image10.jpg" />
         <img src="https://news.aut.ac.nz/__data/assets/image/0006/92328/placeholder-image10.jpg" />
       </div>
-      <br />
-      <h3 style={{ fontWeight: "900px" }}>Want to be Sponsor ?</h3>
 
-      <button class="btn btn-primary p-3 apply-button">APPLY NOW</button>
+      <br />
+
+      <div className="want-to-be-sponser card  p-5 mb-5">
+        <h3 style={{ fontWeight: "900px" }}>Want to be Sponsor ?</h3>
+        <button class="btn btn-primary p-3 apply-button mt-3">APPLY NOW</button>
+
+      </div>
     </div>
   );
 };
