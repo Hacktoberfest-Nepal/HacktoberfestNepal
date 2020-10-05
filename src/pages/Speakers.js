07eff5e7f21069a01f9a78cae0d3a5577215538c
Saroj Dahal <sarojdahal1213@gmail.com>
2020-10-05 16:39:10 +0545

    updated: with speakerList component

--- src/pages/Speakers.js
+++ src/pages/Speakers.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import BreadCrumb from "../components/BreadCrumb";
 import SpeakerList from "../components/speakers/SpeakerList";
 
 const Speakers = () => {
+  useEffect(() => {
+    document.title = "Speakers"
+  }, []);
   return (
     <div>
       <BreadCrumb image="speaker-bg.png" title="Speakers" description="Speakers from expert fields" />
