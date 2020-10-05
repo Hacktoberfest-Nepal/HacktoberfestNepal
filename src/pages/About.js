7f9cdebe432647dfd6377f685da0359dfbed3aa9
Saroj Dahal <sarojdahal1213@gmail.com>
2020-10-05 16:41:22 +0545

    chore:null

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import BreadCrumb from "../components/BreadCrumb";
 import SponsorComponent from "../components/about/Sponsor";
 import OrganizersComponent from "../components/about/Organizers";
 
 const About = () => {
+  useEffect(() => {
+    document.title = "About"
+  }, []);
+
   return (
     <div>
       <BreadCrumb image="about.png" title="About" description="All about Hacktoberfest" />
