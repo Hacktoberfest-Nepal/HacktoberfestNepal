1c55b82861b83347f9ac3e8744d4d8170a536595
Saroj Dahal <sarojdahal1213@gmail.com>
2020-10-05 16:54:54 +0545

    updated: UI

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ContactComp from "../components/ContactComponent";
+import BreadCrumb from "../components/BreadCrumb";
 
 const Contact = () => {
   return (
     <div>
+      <BreadCrumb image="contact.png" title="Contact" description="Feel Free to Contact us" />
       <ContactComp />
     </div>
   );
