5a8458534cffd6b9395c57cf0531c7897a4acaba
Saroj Dahal <sarojdahal1213@gmail.com>
2020-10-05 17:05:16 +0545

    added: info Component

---
+++ src/components/about/Info.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Info = () => {
+    return (
+        <div className="container card p-5">
+
+            <h3>Event Description</h3>
+
+            Hacktoberfest — brought to you by DigitalOcean in partnership with Dev & Intel — is a month-long celebration of open source software. Maintainers are invited to guide would-be contributors towards issues that will help move the project forward, and contributors get the opportunity to give back to both projects they like and others they've just discovered. No contribution is too small — bug fixes and documentation updates are valid ways of participating.
+        </div>
+    )
+}
+
+export default Info
