ebd196c52b914c9b53511b779e25fd71e990729e
Saroj Dahal <sarojdahal1213@gmail.com>
2020-10-05 17:05:38 +0545

    unrequired items removed

--- src/components/about/Sponsor.js
+++ src/components/about/Sponsor.js
@@ -4,28 +4,12 @@ import TitleComponent from "../Title";
 
 const Sponsor = () => {
   return (
-    <div className="container sponsor">
 
-      <TitleComponent
-        title="Sponsors"
-        description="We are grateful to our amazing partners and sponsors"
-      />
 
-      <div className="sponsorList mt-3">
-        <img class="card" src="https://news.aut.ac.nz/__data/assets/image/0006/92328/placeholder-image10.jpg" />
-        <img src="https://news.aut.ac.nz/__data/assets/image/0006/92328/placeholder-image10.jpg" />
-        <img src="https://news.aut.ac.nz/__data/assets/image/0006/92328/placeholder-image10.jpg" />
-        <img src="https://news.aut.ac.nz/__data/assets/image/0006/92328/placeholder-image10.jpg" />
-        <img src="https://news.aut.ac.nz/__data/assets/image/0006/92328/placeholder-image10.jpg" />
-      </div>
+    <div className="container want-to-be-sponser  p-5 mb-5 mt-5">
+      <h3 style={{ fontWeight: "900px" }}>Want to be Sponsor ?</h3><br />
+      <a href="/contact"><button class="btn btn-primary p-3 apply-button mt-3">Contact us</button></a>
 
-      <br />
-
-      <div className="want-to-be-sponser card  p-5 mb-5">
-        <h3 style={{ fontWeight: "900px" }}>Want to be Sponsor ?</h3>
-        <button class="btn btn-primary p-3 apply-button mt-3">APPLY NOW</button>
-
-      </div>
     </div>
   );
 };
