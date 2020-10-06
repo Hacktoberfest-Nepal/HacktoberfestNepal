81c0127fef98c640f05be0c18fa1cce0eb40d3b7
Saroj Dahal <sarojdahal1213@gmail.com>
2020-10-06 07:12:47 +0545

    Updates

---
+++ precache-manifest.38a18998766bc15c3e1fc83e6d805406.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11fcd30fc6d04b693ad89d9dbb2f7a18",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf5bec4f0dc940b2eba7",
+    "url": "/static/css/2.602c902f.chunk.css"
+  },
+  {
+    "revision": "c48dbce802b7ad7717c0",
+    "url": "/static/css/main.8c85b121.chunk.css"
+  },
+  {
+    "revision": "cf5bec4f0dc940b2eba7",
+    "url": "/static/js/2.b96361e7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b96361e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c48dbce802b7ad7717c0",
+    "url": "/static/js/main.383503e8.chunk.js"
+  },
+  {
+    "revision": "df4dfa082f615a0aa391",
+    "url": "/static/js/runtime-main.d53c1ff5.js"
+  },
+  {
+    "revision": "d5b097fc80453883d3f9fe9f6077d69c",
+    "url": "/static/media/2.d5b097fc.jpg"
+  },
+  {
+    "revision": "ef4545fe8c8257d5fb849a4eac75b6f4",
+    "url": "/static/media/about.ef4545fe.png"
+  },
+  {
+    "revision": "7d655c01651e18713f37bc845f3d4711",
+    "url": "/static/media/askbuddie.7d655c01.png"
+  },
+  {
+    "revision": "eb94032cddf047e51ac3ecf6c6d7002b",
+    "url": "/static/media/ayadav.eb94032c.jpg"
+  },
+  {
+    "revision": "0e05d4aab4671292d1678f948e70d698",
+    "url": "/static/media/bghimire.0e05d4aa.jpg"
+  },
+  {
+    "revision": "c374152ca92bd47a8f5cecd1bcbbf81c",
+    "url": "/static/media/bjung.c374152c.jpg"
+  },
+  {
+    "revision": "13d884db7149987da791a59a1f27c7b0",
+    "url": "/static/media/contact.13d884db.png"
+  },
+  {
+    "revision": "c5be65c6aef7add75f302d35c35ff588",
+    "url": "/static/media/csaju.c5be65c6.jpg"
+  },
+  {
+    "revision": "b9e9b73c283129136fe8439a6d206a91",
+    "url": "/static/media/csit.b9e9b73c.jpg"
+  },
+  {
+    "revision": "e5c0bd6ed977ed578ea66d3855108382",
+    "url": "/static/media/eventsnp.e5c0bd6e.jpg"
+  },
+  {
+    "revision": "fbab3bf824b9bfaa8a54d0cdd95848ee",
+    "url": "/static/media/gameLogo.fbab3bf8.jpg"
+  },
+  {
+    "revision": "90e588fff59583de4c4663dd73c444e7",
+    "url": "/static/media/gitGithub.90e588ff.png"
+  },
+  {
+    "revision": "3a5eff2ce82573bb5e10d1a932d8bea0",
+    "url": "/static/media/hacktoberLogo.3a5eff2c.jpg"
+  },
+  {
+    "revision": "fc003e461dd3d5c77cdcc823509165e0",
+    "url": "/static/media/logo.fc003e46.png"
+  },
+  {
+    "revision": "d4a80ddfcacfb7348c10b211bc1afdb1",
+    "url": "/static/media/open-source-500x500.d4a80ddf.png"
+  },
+  {
+    "revision": "c39f9cf4c5ce4a052b01d48a3f11fa0c",
+    "url": "/static/media/publicSpeaker.c39f9cf4.png"
+  },
+  {
+    "revision": "f61fb3b9fdb2d7da3952e15748da59a6",
+    "url": "/static/media/python_nepal.f61fb3b9.jpg"
+  },
+  {
+    "revision": "48fd51e8dcc724eeaa2ce5a3bc0c733e",
+    "url": "/static/media/rashika.48fd51e8.jpg"
+  },
+  {
+    "revision": "751b1bbd1f997b7f5de8c0c342bff156",
+    "url": "/static/media/sdahal.751b1bbd.jpg"
+  },
+  {
+    "revision": "47229d3645b5fe3b451a2a94687d8c38",
+    "url": "/static/media/speaker-bg.47229d36.png"
+  },
+  {
+    "revision": "9ff8609cdad5c0e505875c949e6d2aa7",
+    "url": "/static/media/ssinnathurai.9ff8609c.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/HacktoberfestNepal/precache-manifest.89a0a7c91eacb357cfd3d0b8806b9d6a.js"
+  "/precache-manifest.38a18998766bc15c3e1fc83e6d805406.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/HacktoberfestNepal/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
